f3fd2e90dd7627cca424dd49a3da2ab33fb6a9af
Aaron Lee <kangkang365@gmail.com>
2026-04-21 20:44:16 +0800

    Fix assimp's version issue

--- includes/learnopengl/model.h
+++ includes/learnopengl/model.h
@@ -236,7 +236,7 @@ unsigned int TextureFromFile(const char *path, const string &directory, bool gam
     }
     else
     {
-        std::cout << "Texture failed to load at path: " << path << std::endl;
+        std::cout << "Texture failed to load at path: " << filename << std::endl;
         stbi_image_free(data);
     }
 
